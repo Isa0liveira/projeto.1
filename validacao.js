e1a6640e83a58c9269a299065daecac529a36cdd
Isabella Oliveira <117923301+Isa0liveira@users.noreply.github.com>
2022-12-07 14:19:53 -0300

    Add files via upload

--- validacao.js
+++ validacao.js
@@ -1,28 +1,61 @@
-function validacao() {
-    const validaremail = emailvalido();
-    document.getElementById("botao-login").disabled = !validaremail;
-
-    const validarsenha = senhavalida();
-    document.getElementById("botao-login").disabled = !validaremail || !validarsenha;
-
+function onChangeEmail() {
+    toggleButtonsDisable();
+    toggleEmailErrors();
 }
 
-function emailvalido() {
+function onChangePassword() {
+    toggleButtonsDisable();
+    togglePasswordErrors();
+} 
+
+function toggleEmailErrors() {
+    const email = document.getElementById("email").value;
+    if (!email) {
+        document.getElementById("email-required-error").style.display = "block";
+    } else {
+        document.getElementById("email-required-error").style.display = "none";
+    }
+    
+    if (validateEmail(email)) {
+        document.getElementById("email-invalid-error").style.display = "none";
+    } else {
+        document.getElementById("email-invalid-error").style.display = "block";
+    }
+}
+
+function togglePasswordErrors() {
+    const password = document.getElementById("password").value;
+    if (!password) {
+       document.getElementById("password-required-error").style.display = "block";
+    } else {
+       document.getElementById("password-required-error").style.display = "none";
+    }
+}
+
+function toggleButtonsDisable() {
+    const emailValid = isEmailValid();
+    document.getElementById("recover-password-button").disabled = !emailValid;
+
+    const passwordValid = isPasswordValid();
+    document.getElementById("login-button").disabled = !emailValid || !passwordValid;
+}
+
+function isEmailValid() {
     const email = document.getElementById("email").value;
     if (!email) {
         return false;
     }
-    return validacaoemail(email);
+    return validateEmail(email);
 }
 
-function senhavalida() {
-    const senha = document.getElementById("senha").value;
-    if (!senha) {
+function isPasswordValid() {
+    const password = document.getElementById("password").value;
+    if (!password) {
         return false;
     }
     return true;
 }
 
-function validacaoemail(email) {
+function validateEmail(email) {
     return /\S+@\S+\.\S+/.test(email);
 }
